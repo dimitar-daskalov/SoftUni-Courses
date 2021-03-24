4eacd83f911a1335f26641fc1a883966b85b1d9b
dimitar-daskalov <dimitar.daskalov22@gmail.com>
2021-03-24 22:24:12 +0200

    Added Python OOP Resources

---
+++ python_OOP/labs_and_homeworks/09_decorators_exercise/02_even_parameters.py
@@ -0,0 +1,31 @@
+def even_parameters(func):
+
+    def wrapper(*args):
+        not_evens = [el for el in args if type(el) != int or el % 2 != 0]
+        if not_evens:
+            return "Please use only even numbers!"
+
+        return func(*args)
+
+    return wrapper
+
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+print(add(2, 4))
+print(add("Peter", 1))
+
+
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+    return result
+
+
+print(multiply(2, 4, 6, 8))
+print(multiply(2, 4, 9, 8))
