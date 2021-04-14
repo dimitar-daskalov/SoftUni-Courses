b29be82c737beba7fab41a4efc5b0d25db1fbe4c
dimitar-daskalov <dimitar.daskalov22@gmail.com>
2021-04-14 21:10:18 +0300

    Added some tests for test application

--- test_project/fashion_load_test_project_v2/hardcoded_logic_file.py
+++ test_project/fashion_load_test_project_v2/hardcoded_logic_file.py
@@ -1,14 +1,15 @@
 import os
 import shutil
-# import webbrowser
+
+original_working_directory = r"\Dcew1164prodinf\TRAITEMENTS\SEPARATION_RUN\GJ\Fashion"
 
 
 def open_folder():
-    open(("\\Dcew1164prodinf\TRAITEMENTS\SEPARATION_RUN\GJ\Fashion"))
+    open(original_working_directory)
 
 
 def is_empty():
-    source_directory = "\\Dcew1164prodinf\TRAITEMENTS\SEPARATION_RUN\GJ\Fashion"
+    source_directory = original_working_directory
     file_names = os.listdir(source_directory)
     if not file_names:
         return True
@@ -16,8 +17,8 @@ def is_empty():
 
 
 def move_files():
-    source_directory = "\\Dcew1164prodinf\TRAITEMENTS\SEPARATION_RUN\GJ\Fashion"
-    target_directory = "\\Dcew1164prodinf\TRAITEMENTS\SEPARATION_RUN"
+    source_directory = original_working_directory
+    target_directory = r"\Dcew1164prodinf\TRAITEMENTS\SEPARATION_RUN"
     file_names = os.listdir(source_directory)
     for file_name in file_names:
         shutil.move(os.path.join(source_directory, file_name), target_directory)
